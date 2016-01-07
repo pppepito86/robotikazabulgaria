9d2384ce1d21a5775ad182fded7b018c87c8f3f2
kasapin40 <kasapin40@gmail.com>
2016-01-07 15:51:51 +0200

    add docs for the fist assignment

--- server.go
+++ server.go
@@ -20,6 +20,7 @@ func main() {
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
+	http.Handle("/docs/", http.StripPrefix("/docs/", http.FileServer(http.Dir("docs"))))
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
